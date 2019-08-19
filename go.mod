8cf00394f80435c5a0d2d5198846ac808774fbd5
Matteo Baiguini <matteo@solidarchitectures.com>
2019-08-19 17:53:35 +0200

    Complete refactoring

--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
 module github.com/swisscom/korp
 
 require (
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/kubernetes-sigs/kustomize v2.0.3+incompatible
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+	github.com/sirupsen/logrus v1.4.2
 	github.com/urfave/cli v1.21.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.2.2
--- main.go
+++ main.go
@@ -1,98 +1,148 @@
 package main
 
 import (
-	"log"
 	"os"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/swisscom/korp/actions"
 	"github.com/urfave/cli"
 )
 
+const (
+	version = "0.0.1"
+)
+
+// main -
 func main() {
-	var scanpath, registry, output string
+
+	setLogLevel("debug")
+
+	app := createApp()
+	addCommands(app)
+	execApp(app)
+}
+
+func setLogLevel(levelStr string) {
+
+	level, err := log.ParseLevel(levelStr)
+	if err != nil {
+		log.Error(err)
+	}
+	log.SetLevel(level)
+}
+
+// createApp - Create CLI application
+func createApp() *cli.App {
+
 	app := cli.NewApp()
 	app.Name = "korp"
 	app.Usage = "push images to a corporate registry based on Kubernetes yaml files"
+	app.Version = version
+	return app
+}
 
+// addCommands - Add commands to CLI application
+func addCommands(app *cli.App) {
+
+	var scanPath, registry, output string
 	app.Commands = []cli.Command{
 		{
 			Name:    "scan",
 			Aliases: []string{"s"},
-			Usage:   "collects images referenced in yaml files and creates a kustomization file",
+			Usage:   "collect images referenced in all yaml files in the path and create a kustomization file",
 			Flags: []cli.Flag{
 				cli.StringFlag{
 					Name:        "files, f",
+					Usage:       "path of yaml files to scan (default: current dir)",
+					EnvVar:      "KORP_SCAN_FILES",
 					Value:       ".",
-					Usage:       "path of yaml files to scan",
-					Destination: &scanpath,
+					Required:    false,
+					Destination: &scanPath,
 				},
 				cli.StringFlag{
 					Name:        "registry, r",
+					Usage:       "name of the Docker registry to use (default: 'docker.io')",
+					EnvVar:      "KORP_SCAN_REGISTRY",
 					Value:       "docker.io",
-					Usage:       "name of the corporate registry to use",
+					Required:    false,
 					Destination: &registry,
 				},
 				cli.StringFlag{
 					Name:        "output, o",
+					Usage:       "path of the kustomization file to be written (default: current dir)",
+					EnvVar:      "KORP_SCAN_OUTPUT",
 					Value:       ".",
-					Usage:       "path of the kustomization file to be written",
+					Required:    false,
 					Destination: &output,
 				},
 			},
-			Action: actions.Scan(&scanpath, &registry, &output),
+			Action: actions.Scan(&scanPath, &registry, &output),
 		},
 		{
 			Name:    "pull",
 			Aliases: []string{"p"},
-			Usage:   "pulls referenced images to the local docker registry",
+			Usage:   "pull images listed in the kustomization file to the local Docker registry",
 			Flags: []cli.Flag{
 				cli.StringFlag{
 					Name:        "path, p",
+					Usage:       "path of the kustomization file (default: current dir)",
+					EnvVar:      "KORP_PULL_PATH",
 					Value:       ".",
-					Usage:       "path of the kustomize file",
-					Destination: &scanpath,
+					Required:    false,
+					Destination: &scanPath,
 				},
 			},
-			Action: actions.Pull(&scanpath),
+			Action: actions.Pull(&scanPath),
 		},
 		{
 			Name:    "push",
 			Aliases: []string{"u"},
-			Usage:   "tags images and pushes them to the corporate registry",
+			Usage:   "re-tag original images and push them to the new Docker registry",
 			Flags: []cli.Flag{
 				cli.StringFlag{
 					Name:        "path, p",
+					Usage:       "path of the kustomization file (default: current dir)",
+					EnvVar:      "KORP_PUSH_PATH",
 					Value:       ".",
-					Usage:       "path of the kustomize file",
-					Destination: &scanpath,
+					Required:    false,
+					Destination: &scanPath,
 				},
 				cli.StringFlag{
 					Name:        "registry, r",
+					Usage:       "name of the new Docker registry to push to (default: 'docker.io')",
+					EnvVar:      "KORP_PUSH_REGISTRY",
 					Value:       "docker.io",
-					Usage:       "name of the corporate registry to use",
+					Required:    false,
 					Destination: &registry,
 				},
 			},
-			Action: actions.Push(&scanpath),
+			Action: actions.Push(&scanPath),
 		},
 		{
 			Name:    "patch",
 			Aliases: []string{"a"},
-			Usage:   "patches the image references in all yaml files in the path",
+			Usage:   "patch all yaml files in the path with images tags to the new Docker registry",
 			Flags: []cli.Flag{
 				cli.StringFlag{
 					Name:        "path, p",
+					Usage:       "path of the kustomization file and the yaml files (default: current dir)",
+					EnvVar:      "KORP_PATCH_PATH",
 					Value:       ".",
-					Usage:       "path of the kustomize file and the yaml files",
-					Destination: &scanpath,
+					Required:    false,
+					Destination: &scanPath,
 				},
 			},
-			Action: actions.Patch(&scanpath),
+			Action: actions.Patch(&scanPath),
 		},
 	}
+}
+
+// execApp - Execute CLI application
+func execApp(app *cli.App) {
 
 	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err)
 	}
 }
